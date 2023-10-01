16424a666206f53d45e9e73a8b404c52c3905400
Adam Szymański <adamszymanski27@hotmail.com>
2023-10-01 23:04:08 +0200

    Add main and custom fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <main>
-      <p></p>
+      <div className="md:container md:mx-auto px-3.2 sm:text-base md:text-lg">
+        <p className="font-plateia">KRISTAL CLEAN</p>
+        <p>Kristal clean</p>
+      </div>
     </main>
   );
 }
